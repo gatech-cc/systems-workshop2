ac8b2348985c448a484a1c4c88930b7cc6898a7d
Enrique Saurez <quique7991@gmail.com>
2016-10-04 04:30:59 +0000

    Fix some bugs on the install script

--- install.sh
+++ install.sh
@@ -1,21 +1,45 @@
-sudo -c
-apt-get install -y zookeeper libzookeeper2 zookeeperd zookeeper-bin libzookeeper-mt-dev ant check build-essential autoconf libtool pkg-config
-echo "Install Conservator C++ Zookeeper Wrapper"
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install -y zookeeper libzookeeper2 zookeeperd zookeeper-bin libzookeeper-mt-dev ant check build-essential autoconf libtool pkg-config checkinstall git
+echo "Instaling cmake 3.0+"
 mkdir -p ~/src
 cd ~/src
-git clone https://github.com/rjenkins/conservator.git
+sudo apt-get remove -y cmake
+wget http://www.cmake.org/files/v3.2/cmake-3.2.2.tar.gz
+tar xf cmake-3.2.2.tar.gz
+cd cmake-3.2.2
+./configure
+make
+sudo checkinstall -y --pkgname cmake
+echo "PATH=/usr/local/bin:$PATH" >> ~/.profile
+source ~/.profile
+echo "Installing Conservator C++ Zookeeper Wrapper"
+cd ~/src
+git clone https://github.com/esaurez/conservator.git
 cd conservator
 cmake .
 make 
-make install
+sudo checkinstall -y --pkgname conservator
 cd ~/src
-echo "Install GRPC"
+echo "Installing GRPC"
 git clone -b $(curl -L http://grpc.io/release) https://github.com/grpc/grpc
 cd grpc
 git submodule update --init
 make
-make install
-echo "Install Protobuf"
-cd ~/grpc/third_party/protobuf
+sudo checkinstall grpc
+echo "Installing Protobuf"
+cd ~/src/grpc/third_party/protobuf
 make 
-sudo make install
+sudo checkinstall -y --pkgname protobuf
+echo "Installing GLoc"
+cd ~/src/
+git clone https://github.com/google/glog.git
+cd glog
+mkdir build && cd build
+export CXXFLAGS="-fPIC" 
+cmake ..
+make
+make test
+sudo checkinstall -y --pkgname glog
+echo "export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH" >> ~/.bashrc
+source ~/.bashrc
